a354604f66b66214b34cbb0bbc6d88afc28d8358
Gregory Leroux <lrxgregory@gmail.com>
2025-10-29 16:21:44 +0100

    init project

---
+++ src/config/navigationBar.ts
@@ -0,0 +1,48 @@
+// Navigation Bar
+// ------------
+// Description: The navigation bar data for the website.
+export interface Logo {
+	src: string
+	alt: string
+	text: string
+}
+
+export interface NavSubItem {
+	name: string
+	link: string
+}
+
+export interface NavItem {
+	name: string
+	link: string
+	submenu?: NavSubItem[]
+}
+
+export interface NavAction {
+	name: string
+	link: string
+	style: string
+	size: string
+}
+
+export interface NavData {
+	logo: Logo
+	navItems: NavItem[]
+	navActions: NavAction[]
+}
+
+export const navigationBarData: NavData = {
+	logo: {
+		src: '/logo.svg',
+		alt: 'The tailwind astro theme',
+		text: 'Foxi.'
+	},
+	navItems: [
+		{ name: 'Home', link: '/' },
+		{ name: 'Pricing', link: '/pricing' },
+		{ name: 'Features', link: '/features' },
+		{ name: 'FAQ', link: '/faq' },
+		{ name: 'Contact', link: '/contact' }
+	],
+	navActions: [{ name: 'Try it now', link: '/', style: 'primary', size: 'lg' }]
+}
