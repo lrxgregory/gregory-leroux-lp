cc4def9a0efa58473eb601f715adcccf7e584b8b
Gregory Leroux <lrxgregory@gmail.com>
2025-11-10 18:26:41 +0100

    feat(config): Update Vercel configuration and ignore files; refine build scripts
    - Added .vercelignore to exclude unnecessary files from deployment.
    - Updated astro.config.mjs to configure Vercel adapter and set the site URL.
    - Modified package.json to streamline the build process with a new build:check script.
    - Adjusted button styles in Pricing.astro for consistency and improved user experience.
    - Enhanced Reviews.astro with optional chaining for safer DOM manipulation.
    - Updated import paths in LayoutExample.astro for better module resolution.
    - Removed outdated cookies and privacy policy pages to streamline content.

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,18 @@ import { defineConfig } from "astro/config";
 import partytown from "@astrojs/partytown";
 import tailwind from "@astrojs/tailwind";
 import icon from "astro-icon";
-
 import sitemap from "@astrojs/sitemap";
+import vercel from "@astrojs/vercel";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://foxi.netlify.app/",
+  site: "https://gregoryleroux.com",
+  output: "server",
+  adapter: vercel({
+    webAnalytics: {
+      enabled: true
+    }
+  }),
   integrations: [
     tailwind(),
     icon(),
